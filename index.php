cc55734b0b66e7026c35d5fb5bfeb7e0af9d5440
ANDiGRA <59337706+ANDiGRA@users.noreply.github.com>
2025-05-18 01:56:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/src/Notification.php';
-require_once __DIR__ . '/src/AbstractNotification.php';
-require_once __DIR__ . '/src/EmailNotification.php';
-require_once __DIR__ . '/src/SMSNotification.php';
+
 
 use App\EmailNotification;
 use App\SMSNotification;
